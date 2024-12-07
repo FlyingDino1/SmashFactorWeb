dfce488dd29861b1eadf4ed5adfe8a334f89ac23
Takuto Misawa <misawatakuto0920@gmail.com>
2024-12-07 15:13:40 +0800

    last update

--- src/app/api/pullMatches/route.ts
+++ src/app/api/pullMatches/route.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient();
  
 export async function POST(
-  req: Request
 ) {
   return NextResponse.json(await prisma.general.findFirst({where: {id: 1}, select: {count:true}}))
 }
